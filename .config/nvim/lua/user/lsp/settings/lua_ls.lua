770de46b59554ce328861d4a868c07222f132dd8
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2024-11-23 18:24:48 +0100

    feat: update lua_ls

--- .config/nvim/lua/user/lsp/settings/lua_ls.lua
+++ .config/nvim/lua/user/lsp/settings/lua_ls.lua
@@ -6,10 +6,15 @@ return {
 				globals = { "vim" },
 			},
 			workspace = {
+				checkThirdParty = false,
 				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
+					vim.env.VIMRUNTIME,
+					-- Depending on the usage, you might want to add additional paths here.
+					"${3rd}/luv/library",
+					-- "${3rd}/busted/library",
 				},
+				-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+				-- library = vim.api.nvim_get_runtime_file("", true)
 			},
 		},
 	},
