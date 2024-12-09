2d076b80d80806852422b0efdc8f86da66e13397
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2024-12-09 09:22:08 +0100

    fix: disable nvim dap text hightligting on termination

--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -434,7 +434,7 @@ local spec = {
 		"<cmd>lua require('neotest').summary.toggle()<cr>",
 		desc = "Toggle summary",
 	},
-  {
+	{
 		"<leader>nc",
 		"<cmd>lua require('neotest').run.run({ suite = true, env = { CI = true } })<cr>",
 		desc = "Run all tests with CI",
@@ -515,6 +515,7 @@ local spec = {
 		function()
 			require("dap").terminate()
 			require("dapui").close()
+			require("nvim-dap-virtual-text").toggle()
 		end,
 		desc = "Terminate",
 		nowait = true,
