09a435fc4b028558ec849f3179da73d7a48c074a
Tamerlan Gudabayev <tgudabay@volvocars.com>
2025-02-10 16:27:01 +0100

    remove obsidian

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -212,39 +212,6 @@ local plugins = {
 			"nvim-neotest/neotest-jest",
 		},
 	},
-
-	-- Obsidian
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*", -- recommended, use latest release instead of latest commit
-		lazy = true,
-		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-		event = {
-			-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-			-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-			-- refer to `:h file-pattern` for more examples
-			"BufReadPre "
-				.. vim.fn.expand("~")
-				.. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/Home/*.md",
-			"BufNewFile " .. vim.fn.expand("~") .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/Home/*.md",
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		opts = {
-			ui = {
-				enable = false,
-			},
-			workspaces = {
-				{
-					name = "home",
-					path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/Home",
-				},
-			},
-		},
-	},
 }
 
 -- Setup plugins using lazy.nvim
