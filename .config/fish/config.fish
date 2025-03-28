b3c702831fe4a5e5972ac640686c162a79ed7d78
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2025-03-28 20:43:46 +0100

    chore: add orbstack to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,7 @@ abbr cat 'bat'
 
 starship init fish | source
 
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
