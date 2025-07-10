bda3fe0b58d73e89b7e91330fbd361830ba36465
Tamerlan Gudabayev <tgudabay@volvocars.com>
2025-07-11 01:18:51 +0200

    bunch of useful stuff to be honest

--- .config/nvim/lua/plugins/general.lua
+++ .config/nvim/lua/plugins/general.lua
@@ -1,50 +1,4 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Define plugins
-local plugins = {
-  { "nvim-lua/plenary.nvim" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile" },
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = {}
-  },
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        enabled = true, -- Enable the dashboard
-      },
-      picker = {
-      },
-      explorer = {
-      }
-    }
-  },
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    config = true,
-    cmd = { "ToggleTerm" },
-    event = "VeryLazy",
-  },
+return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
@@ -55,12 +9,7 @@ local plugins = {
     commit = "88343753dbe81c227a1c1fd2c8d764afb8d36269",
     event = { "BufReadPost", "BufNewFile" },
   },
-  { "nvim-lualine/lualine.nvim", event = "VeryLazy" },
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {},
-  },
+  { "nvim-lualine/lualine.nvim",   event = "VeryLazy" },
   {
     "christoomey/vim-tmux-navigator",
     event = "VeryLazy",
@@ -123,7 +72,5 @@ local plugins = {
     },
     event = { "BufReadPost", "BufNewFile" },
   },
-}
 
--- Setup plugins using lazy.nvim
-require("lazy").setup(plugins, {})
+};
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,27 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    lazygit = {},
+    dashboard = { enabled = true },
+    explorer = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    picker = {
+      enabled = true,
+      sources = {
+        explorer = {
+          hidden = true
+        }
+      }
+    },
+    quickfile = { enabled = true },
+    scope = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
