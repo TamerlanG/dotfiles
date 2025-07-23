c233bb2b7e09eaf2931cae06580ad8c22fb73e90
Tamerlan Gudabayev <tgudabay@volvocars.com>
2025-07-23 09:29:30 +0200

    Update Blink configuration to remove 'enabled' field and adjust 'default' sources for 'AvanteInput'
    Signed-off-by: Tamerlan Gudabayev <tgudabay@volvocars.com>

--- .config/nvim/lua/user/format.lua
+++ .config/nvim/lua/user/format.lua
@@ -2,43 +2,52 @@ local conform = require("conform")
 local lint = require("lint")
 
 conform.setup({
-	formatters_by_ft = {
-		javascript = { "prettierd" },
-		typescript = { "prettierd" },
-		javascriptreact = { "prettierd" },
-		typescriptreact = { "prettierd" },
-		svelte = { "prettierd" },
-		css = { "prettierd" },
-		html = { "prettierd" },
-		json = { "prettierd" },
-		yaml = { "prettierd" },
-		markdown = { "prettierd" },
-		graphql = { "prettierd" },
-		lua = { "stylua" },
-		python = { "isort", "black" },
-	},
-	format_on_save = {
-		lsp_fallback = true,
-		async = false,
-		timeout_ms = 500,
-	},
+  formatters_by_ft = {
+    javascript = { "prettierd" },
+    typescript = { "prettierd" },
+    javascriptreact = { "prettierd" },
+    typescriptreact = { "prettierd" },
+    svelte = { "prettierd" },
+    css = { "prettierd" },
+    html = { "prettierd" },
+    json = { "prettierd" },
+    yaml = { "prettierd" },
+    markdown = { "prettierd" },
+    graphql = { "prettierd" },
+    lua = {
+      args = {
+        "--search-parent-directories",
+        "--indent-type Spaces",
+        "--stdin-filepath",
+        "$FILENAME",
+        "-",
+      },
+
+    },
+    python = { "isort", "black" },
+  },
+  format_on_save = {
+    lsp_fallback = true,
+    async = false,
+    timeout_ms = 500,
+  },
 })
 
 lint.linters_by_ft = {
-	javascript = { "eslint_d" },
-	typescript = { "eslint_d" },
-	javascriptreact = { "eslint_d" },
-	typescriptreact = { "eslint_d" },
-	svelte = { "eslint_d" },
-	python = { "pylint" },
+  javascript = { "eslint_d" },
+  typescript = { "eslint_d" },
+  javascriptreact = { "eslint_d" },
+  typescriptreact = { "eslint_d" },
+  svelte = { "eslint_d" },
+  python = { "pylint" },
 }
 
 -- Auto Commands for Linting
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-	group = lint_augroup,
-	callback = function()
-		lint.try_lint()
-	end,
+  group = lint_augroup,
+  callback = function()
+    lint.try_lint()
+  end,
 })
