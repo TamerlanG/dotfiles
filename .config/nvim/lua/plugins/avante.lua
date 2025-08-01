429d1c7fe13be25f98d0a459415d6a2c3f93d7a8
Tamerlan Gudabayev <tgudabay@volvocars.com>
2025-08-01 11:02:26 +0200

    avante use claude sonnet 4 as main model for copilot

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,35 +1,39 @@
 return {
-	"yetone/avante.nvim",
-	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-	-- ⚠️ must add this setting! ! !
-	build = "BUILD_FROM_SOURCE=true make",
-	event = "VeryLazy",
-	version = false, -- Never set this value to "*"! Never!
-	---@module 'avante'
-	---@type avante.Config
-	opts = {
-		-- add any opts here
-		-- for example
-		provider = "copilot",
-		providers = {},
-		windows = {
-			input = {
-				provider = "snacks",
-				provider_opts = {
-					-- Additional snacks.input options
-					title = "Avante Input",
-					icon = " ",
-				},
-			},
-		},
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"MunifTanjim/nui.nvim",
+  "yetone/avante.nvim",
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- ⚠️ must add this setting! ! !
+  build = "BUILD_FROM_SOURCE=true make",
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
+  opts = {
+    -- add any opts here
+    -- for example
+    provider = "copilot",
+    providers = {
+      copilot = {
+        model = "claude-sonnet-4"
+      },
+    },
+    windows = {
+      input = {
+        provider = "snacks",
+        provider_opts = {
+          -- Additional snacks.input options
+          title = "Avante Input",
+          icon = " ",
+        },
+      },
+    },
+  },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
 
-		--- The below dependencies are optional,
-		"folke/snacks.nvim", -- for input provider snacks
-		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-		"zbirenbaum/copilot.lua", -- for providers='copilot'
-	},
+    --- The below dependencies are optional,
+    "folke/snacks.nvim",           -- for input provider snacks
+    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+    "zbirenbaum/copilot.lua",      -- for providers='copilot'
+  },
 }
