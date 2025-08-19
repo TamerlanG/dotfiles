05b2f4a3c7bba00bb3a4d9dd4633c3c21a9372ad
Tamerlan Gudabayev <tgudabay@volvocars.com>
2025-08-19 18:23:49 +0200

    move configs around

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,77 @@
+return {
+  {
+    "saghen/blink.cmp",
+    version = "1.*",
+    build = "cargo build --release",
+    event = "InsertEnter",
+    dependencies = {
+      "fang2hou/blink-copilot",
+    },
+    opts = {
+      keymap = { preset = "default" },
+      fuzzy = { implementation = "rust" },
+      sources = {
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+          avante = {
+            module = "blink-cmp-avante",
+            name = "Avante",
+          },
+        },
+        default = function()
+          if vim.bo.filetype == "AvanteInput" then
+            return { "avante" }
+          end
+          return { "copilot", "lsp", "path", "buffer" }
+        end,
+      },
+      completion = { documentation = { auto_show = true }, ghost_text = { enabled = true } },
+      appearance = {
+        nerd_font_variant = "mono",
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        kind_icons = {
+          Copilot = "",
+          Text = "󰉿",
+          Method = "󰊕",
+          Function = "󰊕",
+          Constructor = "󰒓",
+
+          Field = "󰜢",
+          Variable = "󰆦",
+          Property = "󰖷",
+
+          Class = "󱡠",
+          Interface = "󱡠",
+          Struct = "󱡠",
+          Module = "󰅩",
+
+          Unit = "󰪚",
+          Value = "󰦨",
+          Enum = "󰦨",
+          EnumMember = "󰦨",
+
+          Keyword = "󰻾",
+          Constant = "󰏿",
+
+          Snippet = "󱄽",
+          Color = "󰏘",
+          File = "󰈔",
+          Reference = "󰬲",
+          Folder = "󰉋",
+          Event = "󱐋",
+          Operator = "󰪚",
+          TypeParameter = "󰬛",
+        },
+      },
+    }
+  }
+
+}
