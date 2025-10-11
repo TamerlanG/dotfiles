48bb207334d7396e07eaa5d2c64b89ce4dcf4454
Tamerlan Gudabayev <tgudabay@volvocars.com>
2025-10-12 00:43:50 +0200

    eagerload them all

--- .config/nvim/lua/user/dap/init.lua
+++ .config/nvim/lua/user/dap/init.lua
@@ -11,25 +11,12 @@ function M.setup()
   dap.listeners.before.event_terminated.user_dap = function() ui.close() end
   dap.listeners.before.event_exited.user_dap = function() ui.close() end
 
-  -- Autoload adapters/configs on file open
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "go", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-    callback = function(ev)
-      require("user.dap.loader").load(ev.match)
-    end,
-  })
+  -- Eagerly load all adapters & configs (no autoload)
+  pcall(require, "user.dap.adapters.go")
+  pcall(require, "user.dap.config.go")
+  pcall(require, "user.dap.adapters.js")
+  pcall(require, "user.dap.config.js")
 
-  -- Also load currently open buffers matching filetypes (if config run after they exist)
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.api.nvim_buf_is_loaded(buf) then
-      local ft = vim.bo[buf].filetype
-      if ft ~= "" and ft:match("go") or ft:match("javascript") or ft:match("typescript") then
-        require("user.dap.loader").load(ft)
-      end
-    end
-  end
-
-  -- UI setup after dap core is present
   require("user.dap.ui").setup()
 end
 
