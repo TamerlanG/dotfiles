6d1054557b9dd17c61dff54d1b6621ec97fff946
Tamerlan Gudabayev <tgudabay@volvocars.com>
2025-10-12 07:23:32 +0200

    update dap hotkeys

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -10,4 +10,41 @@ return {
   config = function()
     require("user.dap").setup()
   end,
+  keys = {
+    -- Core stepping
+    { "<leader>dc", function() require("dap").continue() end,          desc = "Continue / Start" },
+    { "<leader>do", function() require("dap").step_over() end,         desc = "Step Over" },
+    { "<leader>di", function() require("dap").step_into() end,         desc = "Step Into" },
+    { "<leader>du", function() require("dap").step_out() end,          desc = "Step Out" },
+
+    -- Breakpoints
+    { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+    {
+      "<leader>dB",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Condition: "))
+      end,
+      desc = "Conditional Breakpoint"
+    },
+
+    -- Flow control
+    { "<leader>dr", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+    { "<leader>ds", function() require("dap").restart() end,       desc = "Restart Debugger" },
+    { "<leader>dx", function() require("dap").terminate() end,     desc = "Stop Debugger" },
+
+    -- Evaluate / Inspect
+    {
+      "<leader>de",
+      function()
+        vim.ui.input({ prompt = "Evaluate expression: " }, function(expr)
+          if expr then require("dap").repl.execute(expr) end
+        end)
+      end,
+      desc = "Evaluate Expression"
+    },
+
+    -- UI integration
+    { "<leader>dd", function() require("dapui").toggle() end,        desc = "Toggle DAP UI" },
+    { "<leader>df", function() require("dapui").float_element() end, desc = "Float DAP Element" },
+  },
 }
