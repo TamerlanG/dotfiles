4c70bdedb03741f8a1925c30a20bb89e9133d817
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2025-10-28 17:50:44 +0100

    build on save

--- .config/nvim/lsp/zig.lua
+++ .config/nvim/lsp/zig.lua
@@ -9,4 +9,9 @@ return {
   filetypes = { 'zig', 'zir' },
   root_markers = { 'zls.json', 'build.zig', '.git' },
   workspace_required = false,
+  settings = {
+    zls = {
+      enable_build_on_save = true,
+    }
+  }
 }
