6f67e432c6df277ef36df528f38858de9519417e
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2025-11-23 23:26:48 +0100

    feat: add gh keymaps

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -108,6 +108,10 @@ return {
       end,
       desc = "Git Log File",
     },
+    { "<leader>gi", function() Snacks.picker.gh_issue() end,                  desc = "GitHub Issues (open)" },
+    { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+    { "<leader>gp", function() Snacks.picker.gh_pr() end,                     desc = "GitHub Pull Requests (open)" },
+    { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end,    desc = "GitHub Pull Requests (all)" },
 
     -- Search
     {
