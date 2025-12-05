e226cc38e5b6b7badf09b30c7e979b0068b3e06d
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2025-12-06 00:31:27 +0100

    feat: ansible file detection

--- .config/nvim/ftdetect/ansible.lua
+++ .config/nvim/ftdetect/ansible.lua
@@ -1,6 +1,6 @@
 vim.filetype.add({
   pattern = {
     ["roles/tasks/*.{yml,yaml}"] = "yaml.ansible",
-    ["*play*.{yml,yaml}"] = "yaml.ansible",
+    [".*/ansible/.*%.ya?ml"] = "yaml.ansible",
   },
 })
