3fee600d1dcbc5e1a73f58712b9652a1e91f5a3f
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2025-12-26 22:54:20 +0100

    feat: add racket lsp

---
+++ .config/nvim/lsp/racket.lua
@@ -0,0 +1,4 @@
+return {
+  cmd = { 'racket', '-l', 'racket-langserver' },
+  filetypes = { 'racket' },
+}
--- .config/nvim/lua/user/lsp.lua
+++ .config/nvim/lua/user/lsp.lua
@@ -55,4 +55,6 @@ end
 
 
 vim.diagnostic.config(config)
-vim.lsp.enable({ "typescript", "lua_ls", "yaml", "python", "go", "zig", "gh_actions", "terraform", "ansible", "c" })
+vim.lsp.enable(
+  { "typescript", "lua_ls", "yaml", "python", "go", "zig", "gh_actions", "terraform", "ansible", "c",
+    "racket" })
