9cd1b034d121c66d6da65e8f41cdfcb5febb3520
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2026-03-29 23:33:57 +0200

    fix neotest config: move setup into lazy.nvim plugin spec

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -1,10 +1,26 @@
 return {
   "nvim-neotest/neotest",
+  cmd = { "Neotest", "NeotestSummary", "NeotestOutput" },
   dependencies = {
     "nvim-neotest/nvim-nio",
     "nvim-lua/plenary.nvim",
     "antoinemadec/FixCursorHold.nvim",
     "nvim-treesitter/nvim-treesitter",
     "nvim-neotest/neotest-jest",
-  }
+  },
+  config = function()
+    require("neotest").setup({
+      summary = {
+        open = "botright vsplit | vertical resize 80",
+      },
+      adapters = {
+        require("neotest-jest")({
+          jestCommand = "yarn test:jest --",
+          cwd = function(path)
+            return vim.fn.getcwd()
+          end,
+        }),
+      },
+    })
+  end,
 }
