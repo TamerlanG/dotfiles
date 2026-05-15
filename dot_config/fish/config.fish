ab2c6b45bc982be92b2c6246297c0f2c79cec5bb
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2026-05-16 00:44:58 +0200

    add git aliases

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,6 +4,37 @@ abbr ls 'lsd'
 abbr cat 'bat'
 alias k='kubectl'
 
+# Git basics
+abbr g 'git'
+abbr gs 'git status'
+abbr ga 'git add'
+abbr gc 'git commit'
+abbr gcm 'git commit -m'
+abbr gp 'git push'
+abbr gl 'git pull'
+abbr gf 'git fetch'
+abbr glog 'git log --oneline --graph'
+
+# Branching
+abbr gco 'git checkout'
+abbr gcb 'git checkout -b'
+abbr gb 'git branch'
+abbr gm 'git merge'
+abbr grb 'git rebase'
+
+# Diff & stash
+abbr gd 'git diff'
+abbr gds 'git diff --staged'
+abbr gst 'git stash'
+abbr gstp 'git stash pop'
+
+# Remote / fork workflow
+abbr gra 'git remote add'
+abbr grv 'git remote -v'
+
+# GitHub CLI
+abbr ghpr 'gh pr checkout'
+abbr ghsync 'gh repo sync'
 
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
