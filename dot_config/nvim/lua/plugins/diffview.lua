3c79755641e38aec81240fc42aeb25d02e07c2a7
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2026-05-20 20:50:51 +0200

    add diffview

---
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,44 @@
+return {
+  "sindrets/diffview.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = {
+    "DiffviewOpen",
+    "DiffviewClose",
+    "DiffviewToggleFiles",
+    "DiffviewFocusFiles",
+    "DiffviewRefresh",
+    "DiffviewFileHistory",
+  },
+  keys = {
+    {
+      "<leader>gv",
+      function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end,
+      desc = "Diffview Toggle",
+    },
+    {
+      "<leader>gV",
+      "<cmd>DiffviewFileHistory %<cr>",
+      desc = "Diffview File History (current file)",
+    },
+    {
+      "<leader>gH",
+      "<cmd>DiffviewFileHistory<cr>",
+      desc = "Diffview File History (repo)",
+    },
+  },
+  opts = {
+    enhanced_diff_hl = true,
+    view = {
+      merge_tool = {
+        layout = "diff3_mixed",
+        disable_diagnostics = true,
+      },
+    },
+  },
+}
