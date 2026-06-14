c3013b9353095db89b38c230b626f5953675b5d8
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2026-06-14 22:46:50 +0200

    better debugger

--- dot_config/nvim/lua/user/dap/ui.lua
+++ dot_config/nvim/lua/user/dap/ui.lua
@@ -18,7 +18,13 @@ function M.setup()
     })
   end
 
-  require("nvim-dap-virtual-text").setup()
+  require("nvim-dap-virtual-text").setup(
+    {
+      enabled = true,          -- enable this plugin (the default)
+      enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+      virt_text_pos = 'inline'
+    }
+  )
 end
 
 return M
