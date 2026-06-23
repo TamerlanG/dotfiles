682a50a0f0b1cd837a51cb52a02615718cd46bb9
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2026-06-23 09:57:35 +0200

    update

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "javascript" }, -- only what you actually use
     highlight = { enable = true },
   },
   event = { "BufReadPost", "BufNewFile" },
