c7bf50e106edd8bb07d68c91db337b04f2da09f1
Tamerlan Gudabayev <tamerlan@gudabayev.com>
2026-09-10 22:12:05 +0200

    Add macOS system defaults and Touch ID sudo

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -32,6 +32,78 @@
   system.stateVersion = 6;
   system.primaryUser = "tamerlan";
 
+  system.activationScripts.postActivation.text = ''
+    sudo --user=tamerlan -- mkdir -p /Users/tamerlan/Pictures/Screenshots
+  '';
+
+  security.pam.services.sudo_local.touchIdAuth = true;
+
+  system.defaults = {
+    dock = {
+      autohide = true;
+      autohide-delay = 0.0;
+      autohide-time-modifier = 0.2;
+      show-recents = false;
+      mru-spaces = false;
+      expose-group-apps = true;
+      tilesize = 48;
+    };
+
+    finder = {
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      ShowPathbar = true;
+      ShowStatusBar = true;
+      FXPreferredViewStyle = "Nlsv";
+      FXEnableExtensionChangeWarning = false;
+      _FXShowPosixPathInTitle = true;
+      _FXSortFoldersFirst = true;
+      NewWindowTarget = "Home";
+    };
+
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      AppleKeyboardUIMode = 3;
+      ApplePressAndHoldEnabled = false;
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      NSWindowShouldDragOnGesture = true;
+      "com.apple.swipescrolldirection" = true;
+    };
+
+    trackpad = {
+      Clicking = true;
+      TrackpadThreeFingerDrag = true;
+    };
+
+    WindowManager.EnableStandardClickToShowDesktop = false;
+
+    LaunchServices.LSQuarantine = false;
+
+    loginwindow.GuestEnabled = false;
+
+    screencapture = {
+      location = "/Users/tamerlan/Pictures/Screenshots";
+      type = "png";
+      disable-shadow = true;
+    };
+
+    CustomUserPreferences = {
+      "com.apple.desktopservices" = {
+        DSDontWriteNetworkStores = true;
+        DSDontWriteUSBStores = true;
+      };
+      "com.apple.AdLib".allowApplePersonalizedAdvertising = false;
+    };
+  };
+
   nixpkgs.hostPlatform = "aarch64-darwin";
   nixpkgs.config.allowUnfreePredicate =
     pkg:
